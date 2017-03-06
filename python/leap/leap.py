6e37d51a1df45de02d60f6679e509277ec4f1602
Sean M Hamlet <seanmhamlet@gmail.com>
2017-03-06 16:51:06 -0500

    Simplifies solution and makes more readable

--- python/leap/leap.py
+++ python/leap/leap.py
@@ -1,13 +1,16 @@
 """
 leap - Given a year, report if it is a leap year.
+
+The tricky thing here is that a leap year in the
+Gregorian calendar occurs:
+
+on every year that is evenly divisible by 4
+  except every year that is evenly divisible by 100
+    unless the year is also evenly divisible by 400
 """
 
 def is_leap_year(year):
-    if year % 4 == 0:
-        if year % 100 == 0:
-            if year % 400 == 0:
-                return True
-            else:
-                return False
-        return True
-    return False
+    divisible_by_4   = year % 4 == 0
+    divisible_by_100 = year % 100 == 0
+    divisible_by_400 = year % 400 == 0
+    return (divisible_by_4 and not divisible_by_100) or divisible_by_400
