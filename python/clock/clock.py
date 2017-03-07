0a36516000731d5593b49c34649c76c0fbb3a722
Sean M Hamlet <seanmhamlet@gmail.com>
2017-03-07 10:53:48 -0500

    Simplifies Clock object Previously, there were a lot of extra methods to make the code more readable. However, once I learned you could return a new object instance in the add method (which is inclusive of the added minutes), then there was no need for a make_string function, set_time function, or any other helper function. This new version is simpler and more readable.

--- python/clock/clock.py
+++ python/clock/clock.py
@@ -5,32 +5,18 @@ clock - create clock independent of date
 import math
 
 class Clock:
-
     def __init__(self, hours, minutes):
-        # convert time to minutes
-        minutes = hours * 60 + minutes
-        self.set_hours_minutes(minutes)
-
-    # adds minutes and returns str of time
-    def add(self, minutes_to_add):
-        # convert current time to minutes with addition of new minutes
-        minutes = self.hours * 60 + self.minutes + minutes_to_add
-        self.set_hours_minutes(minutes)
-        return self.make_time_string(self.hours,self.minutes)
-
-    def set_hours_minutes(self, minutes):
-        self.hours = math.floor(minutes / 60) % 24
+        self.hours = (hours + math.floor(minutes / 60)) % 24
         self.minutes = minutes % 60
 
     # returns string representation of object
     def __str__(self):
-        return self.make_time_string(self.hours,self.minutes)
+        return "{:02d}:{:02d}".format(self.hours, self.minutes)
 
     # define equality between two objects
     def __eq__(self, other):
         return self.hours == other.hours and self.minutes == other.minutes
 
-    # defines time string
-    @staticmethod
-    def make_time_string(hours, minutes):
-        return "{:02d}:{:02d}".format(hours,minutes)
+    # adds minutes returns new clock object
+    def add(self, minutes_to_add):
+        return Clock(self.hours, self.minutes + minutes_to_add)
