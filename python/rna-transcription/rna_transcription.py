46eb8c5bd1edf51b6ba99ae99be63acfd6d43ee4
Sean M Hamlet <seanmhamlet@gmail.com>
2017-03-08 08:26:33 -0500

    Uses object itself instead of function of object

--- python/rna-transcription/rna_transcription.py
+++ python/rna-transcription/rna_transcription.py
@@ -2,18 +2,19 @@
 rna_transcription - Given a DNA strand, return its RNA complement
                     (per RNA transcription).
 """
-def to_rna(dna):
-    # validate input (only contains GCTA)
-    valid = set('GCTA')
-    if set(dna) <= valid:
-        return ''.join([complement(nuc) for nuc in dna])
-    else:
-        return ''
 
-def complement(nuc):
-    return {
+def to_rna(dna):
+    complement = {
         'G': 'C',
         'C': 'G',
         'T': 'A',
         'A': 'U'
-    }.get(nuc,'') # default case is '' if can't find nuc in dict
+    }
+
+    # validate input (only contains GCTA)
+    valid = set('GCTA')
+
+    if set(dna) <= valid:
+        return ''.join([complement[nuc] for nuc in dna])
+    else:
+        return ''
