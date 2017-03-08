f9a19922b79f2c416323d3ed11a5fe9182200e5d
Sean M Hamlet <seanmhamlet@gmail.com>
2017-03-08 09:08:52 -0500

    Simplifies solution using list comprehension and zipped lists

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -5,8 +5,5 @@ hamming - Calculate the Hamming difference between two DNA strands
 def distance(dna_1, dna_2):
     if len(dna_1) != len(dna_2):
         raise ValueError('DNA strands not same length!')
-    diff_list = []
-    for i in range(len(dna_1)):
-        if dna_1[i] != dna_2[i]:
-            diff_list.append(dna_1[i])
-    return len(diff_list)
+    else:
+        return sum([d1 != d2 for d1, d2 in zip(dna_1, dna_2)])
