cb60623b3e89e236b99970cdc3943658702cc3fd
Sean M Hamlet <seanmhamlet@gmail.com>
2017-03-09 15:33:50 -0500

    Simplifies logic to just OR statement

--- javascript/hello-world/hello-world.js
+++ javascript/hello-world/hello-world.js
@@ -7,7 +7,7 @@
 var HelloWorld = function() {};
 
 HelloWorld.prototype.hello = function(input) {
-  return "Hello, " + (input ? input : "World") + "!"
+  return "Hello, " + (input || "World") + "!"
 };
 
 module.exports = HelloWorld;
