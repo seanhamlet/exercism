8ce6cbb6188d35ebe25b87837b342a78b9ca3306
Sean M Hamlet <seanmhamlet@gmail.com>
2017-03-09 16:32:05 -0500

    Initial solution for leap year exercise

--- javascript/leap/leap.js
+++ javascript/leap/leap.js
@@ -1,14 +1,17 @@
-//
-// This is only a SKELETON file for the "Leap" exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+/*
+leap - Given a year, report if it is a leap year.
+*/
 
-var Year = function() {};
+var Year = function(year) {
+  this.year = year;
+};
 
-Year.prototype.isLeap = function(input) {
-//
-// YOUR CODE GOES HERE
-//
+Year.prototype.isLeap = function() {
+  divisibleBy4   = this.year % 4 === 0;
+  divisibleBy100 = this.year % 100 === 0;
+  divisibleBy400 = this.year % 400 === 0;
+
+  return (divisibleBy4 && !divisibleBy100) || divisibleBy400;
 };
 
 module.exports = Year;
