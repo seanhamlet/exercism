94d5d5635ced98205be81a6081a82c78fdf0987b
Sean M Hamlet <seanmhamlet@gmail.com>
2017-03-09 16:59:22 -0500

    Updates error thrown to be new

--- javascript/hamming/hamming.js
+++ javascript/hamming/hamming.js
@@ -6,7 +6,7 @@ var Hamming = function() {};
 
 Hamming.prototype.compute = function(dna1, dna2) {
   if (dna1.length !== dna2.length) {
-    throw Error('DNA strands must be of equal length.');
+    throw new Error('DNA strands must be of equal length.');
   }
 
   var distance = 0;
