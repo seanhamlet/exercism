1e1952b268591bee06ca03997404a416251a9e0c
Sean M Hamlet <seanmhamlet@gmail.com>
2017-03-09 16:57:08 -0500

    Initial solution to hamming exercise

---
+++ javascript/hamming/hamming.js
@@ -0,0 +1,24 @@
+/*
+hamming - Calculate the Hamming difference between two DNA strands.
+*/
+
+var Hamming = function() {};
+
+Hamming.prototype.compute = function(dna1, dna2) {
+  if (dna1.length !== dna2.length) {
+    throw Error('DNA strands must be of equal length.');
+  }
+
+  var distance = 0;
+
+  // treat string as array and loop through comparing each value
+  for (var i = 0; i < dna1.length; i++) {
+      if (dna1[i] !== dna2[i]) {
+        distance++;
+      }
+  }
+
+  return distance;
+}
+
+module.exports = Hamming;
