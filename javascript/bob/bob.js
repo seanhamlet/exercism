7796b39d1269e14b4dd0216a9e3c1c35e2526c3e
Sean M Hamlet <seanmhamlet@gmail.com>
2017-03-10 11:18:31 -0500

    Simplifies solution logic and variables

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -5,27 +5,26 @@ bob - a lackadasical teenager
 var Bob = function() {};
 
 Bob.prototype.hey = function(input) {
+  // remove extra whitespace
+  input = input.trim();
 
-   var isUpperCase      = input === input.toUpperCase();
-   var containsAlpha    = input.match(/[a-z]/i)
-   var isShouting       = isUpperCase && containsAlpha;
-   var endsWithQuestion = input.charAt(input.length - 1) === '?';
-   // '+' removes contiguous spaces, not just char by char.
-   var isEmpty          = input.replace(/\s+/g, '') === '';
+  var isShouting = input === input.toUpperCase() && input.match(/[a-z]/i);
+  var endsWithQuestion = input.endsWith('?');
+  var isEmpty = input.length === 0;
 
-   if (isShouting) {
-     // if shouting (uppercase letters)
-     return 'Whoa, chill out!';
-   } else if (endsWithQuestion) {
-     // question
-     return 'Sure.';
-   } else if (isEmpty) {
+  if (isShouting) {
+    // if shouting (uppercase letters)
+    return 'Whoa, chill out!';
+  } else if (endsWithQuestion) {
+   // question
+    return 'Sure.';
+  } else if (isEmpty) {
      // if string empty or space
-     return 'Fine. Be that way!';
-   } else {
+    return 'Fine. Be that way!';
+  } else {
      // default
-     return 'Whatever.';
-   }
+    return 'Whatever.';
+  }
 };
 
 module.exports = Bob;
