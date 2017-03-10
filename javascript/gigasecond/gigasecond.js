1f712a2e899644c47e7e9e329f26687ff0e9ee4a
Sean M Hamlet <seanmhamlet@gmail.com>
2017-03-10 13:36:21 -0500

    Simplifies by removing excess unused variables

--- javascript/gigasecond/gigasecond.js
+++ javascript/gigasecond/gigasecond.js
@@ -2,22 +2,13 @@
 gigasecond - calculate the moment when someone has lived for 10^9 years
 */
 
-var Gigasecond = function(startDate) {
-  this.startDate = startDate;
+var Gigasecond = function(date) {
+  this.start = date;
 };
 
 Gigasecond.prototype.date = function() {
-  // define gigamillisecond
-  var gigamillisecond =  Math.pow(10,9) * Math.pow(10,3);
-
-  // clone original date so as to not affect original
-  this.gsDate = new Date(this.startDate.getTime());
-
-  // set time of gsDate
-  this.gsDate.setTime(this.gsDate.getTime() + gigamillisecond);
-
-  // return gsDate
-  return this.gsDate;
+  // add 10^9 seconds to date and return new date
+  return new Date(this.start.getTime() + 1e9 * Math.pow(10,3));
 }
 
 module.exports = Gigasecond;
